7b1d0973ab6af852a34aa022c7be130320847dc2
e-leal <leal.elyssa03@gmail.com>
2020-06-28 23:00:24 -0500

    Improve weekly forecast layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-var innerTodayContent = "";
-var innerWeekContent = "";
+
 function  citySearch(){
+   // var innerTodayContent = "";
     var searchEl = document.getElementById("cityContainer");
     var cityVal = document.getElementById("searchCity").value;
     var weekEl = document.getElementById("weekWeather");
-    innerWeekContent = "";
     weekEl.innerHTML = "";
-    console.log(cityVal);
+    //console.log(cityVal);
     if(cityVal.length ===  0){
         alert("Please enter a valid city");
     }
@@ -14,23 +13,38 @@ function  citySearch(){
     var todayEl = document.getElementById("todayWeather");
     var todayDate = moment().format('l');
     document.getElementById("searchCity").value = "";
-    var cityEl = document.createElement("div");
-    cityEl.className = "card pt-2 pb-2 pl-2";
+    var cityEl = document.createElement("li");
+    cityEl.className = "list-group-item searchedCity";
     cityEl.innerHTML = cityVal;
     searchEl.appendChild(cityEl);
     //todayEl.innerHTML = "<h2>"+cityVal+ " ("+todayDate+")</h2>";
-    
+    displayWeather(cityVal);
+}
+
+
+
+function clearResults(){
+    var weekEl = document.getElementById("weekWeather");
+    weekEl.innerHTML = "";
+    var todayEl = document.getElementById("todayWeather");
+    todayEl.innerHTML = "";
+}
+
+function displayWeather(selectedCity){
+    var innerTodayContent = "";
+    var innerWeekContent = "";
+    var todayDate = moment().format('l');
+    var weekEl = document.getElementById("weekWeather");
+    var todayEl = document.getElementById("todayWeather");
     fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=' + cityVal + '&appid=0d3489588c8dc1cc4818f8f7817f20e1&units=imperial'
+        'https://api.openweathermap.org/data/2.5/weather?q=' + selectedCity + '&appid=0d3489588c8dc1cc4818f8f7817f20e1&units=imperial'
     )
     .then(function(todayResponse){
-        console.log(todayResponse);
         return todayResponse.json();
     })
     .then(function(todayResponse){
-        console.log(todayResponse.weather[0].icon);
         var weatherIcon = todayResponse.weather[0].icon;
-        innerTodayContent = "<h2>"+cityVal+ " ("+todayDate+")" + '<img src="http://openweathermap.org/img/w/'+ weatherIcon + '.png" /></h2>';
+        innerTodayContent = "<h2>"+selectedCity+ " ("+todayDate+")" + '<img src="http://openweathermap.org/img/w/'+ weatherIcon + '.png" /></h2>';
         var tempVal = todayResponse.main.temp;
         innerTodayContent += "<p>Temperature: " + tempVal + "°F</p>";
         var humidityVal = todayResponse.main.humidity;
@@ -39,7 +53,7 @@ function  citySearch(){
         innerTodayContent +="<p>Wind Speed: " + windSpVal + " MPH</p>";
         todayEl.innerHTML = innerTodayContent;
         return fetch(
-            'https://api.openweathermap.org/data/2.5/forecast?q='+ cityVal+'&appid=0d3489588c8dc1cc4818f8f7817f20e1&units=imperial'
+            'https://api.openweathermap.org/data/2.5/forecast?q='+ selectedCity+'&appid=0d3489588c8dc1cc4818f8f7817f20e1&units=imperial'
         )
     })
     .then(function(response){
@@ -48,20 +62,33 @@ function  citySearch(){
     })
     .then(function(response){
         //console.log("Where our temp is supposed to be read: " + response.list[0].main.temp);
+        innerWeekContent += '<h2 class="w-100 mt-3">5-Day Forecast:</h2>';
         for(var i = 0; i< 5; i++){
             var date = moment().add(i+1,"day").format("l");
             var temp = response.list[i].main.temp;
             var humidity = response.list[i].main.humidity;
             var wind = response.list[i].wind.speed;
             var icon = response.list[i].weather[0].icon;
-            innerWeekContent += '<div class="card ml-3 bg-primary text-white p-2"><h3>'+ date +'</h3>'+
+            innerWeekContent += '<div class="card mr-2 bg-primary text-white p-2"><h3>'+ date +'</h3>'+
             '<img src="http://openweathermap.org/img/w/'+ icon+'.png" class="w-25" /><p>Temperatue: '+temp+'°F<br/>Humidity: '+
-            humidity+'%<br/>Wind Speed: '+ wind +'MPH</p></div>';
+            humidity+'%</p></div>';
         }
         weekEl.innerHTML = innerWeekContent;
     })
 }
 
-function savedSearch(){
-
+function savedSearch(savedCity){
+    console.log("Entered the savedSearch function");
+    clearResults();
+    /* var weekEl = document.getElementById("weekWeather");
+    var todayEl = document.getElementById("todayWeather"); */
+    console.log(this);
+    //var citySelected = savedCity;
+    console.log(savedCity);
+    displayWeather(savedCity);
 }
+
+$(".searchedCity").on("click", function(){
+    console.log("Trying to read history search: ", this);
+    savedSearch($(this).text());
+});
